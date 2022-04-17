3b917d24e9b6bad9a8940cdc87e5398f180d8a74
Kaloian Tsenkulovski <kaloian.cenkulovski_5@abv.bg>
2022-04-17 17:03:52 +0300

    job info

--- src/components/Jobs/index.js
+++ src/components/Jobs/index.js
@@ -10,8 +10,8 @@ const Jobs = () => {
         <div>
           <h2>My Work Experience</h2>
           <div className='info'>
-          <p>I've been working in Grind Web Studio since March 2022. The new technologys I had experience with was PHP, Laravel, Local, WordPress and especially how BackEnd and FrontEnd have collaboration in the same project.
-            Since it is my first job I've learned how people work and how to work in a team. 
+          <p>I've been working in Grind Web Studio since March 2022. The new technologies I had experience with were PHP, Laravel, Local, WordPress and especially how BackEnd and FrontEnd have collaboration in the same project.
+            Since it is my first job I've learned how people work and how to be part of a team. 
           </p>
           </div>
         </div>
